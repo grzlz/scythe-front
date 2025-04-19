25057d49f9dc952ed7982f4b12a7634287ead7f9
grzlz <guillermo.rdzlpz@gmail.com>
2025-04-19 00:04:05 -0600

    Add new database credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zspvjlnobpuiyirqogws.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzcHZqbG5vYnB1aXlpcnFvZ3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2ODQ4ODMsImV4cCI6MjA2MDI2MDg4M30.OTx0jPZfSX9FtFndntWTh6dCB-FiNGINuxBAaScEVqA'
+const supabaseUrl = 'https://fqowfwuhdpyewwgezsyt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxb3dmd3VoZHB5ZXd3Z2V6c3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDE2ODcsImV4cCI6MjA2MDYxNzY4N30.C0EztBcnBbg8njnEXwEefQwEbwA4G55TsnSV_ekyaaQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
