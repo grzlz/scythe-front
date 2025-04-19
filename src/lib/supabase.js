be338e96bfadfc1c05bb7c97a49b27398b191ffa
grzlz <guillermo.rdzlpz@gmail.com>
2025-04-19 00:34:07 -0600

    Fix database url and password

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fqowfwuhdpyewwgezsyt.supabase.co'
+const supabaseUrl = 'https://zspvjlnobpuiyirqogws.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzcHZqbG5vYnB1aXlpcnFvZ3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2ODQ4ODMsImV4cCI6MjA2MDI2MDg4M30.OTx0jPZfSX9FtFndntWTh6dCB-FiNGINuxBAaScEVqA'
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
