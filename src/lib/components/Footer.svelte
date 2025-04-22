2eb611bd8628a867411c3dddb6a37126a0232405
grzlz <guillermo.rdzlpz@gmail.com>
2025-04-21 22:27:48 -0600

    Modify footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,14 @@
 <footer class="footer mt-auto py-4 bg-light text-center">
     <div class="container">
-      <img src="/icarus_logo.png" alt="Logo" style="height: 40px;" class="mb-2" />
-      <div>
-        <a href="/auth/register" class="text-decoration-none text-dark">Súmate</a>
-      </div>
+      <a href="/auth/register" class="text-decoration-none text-dark">Súmate</a>
+      <img src="/icarus_logo.png" alt="Logo" style="height: 30px;" class="mb-2" />
     </div>
   </footer>
   
   <style>
     .footer {
       box-shadow: 0 -2px 4px rgba(0,0,0,0.1);
-      margin-top: 3rem;
+
     }
   </style>
   
